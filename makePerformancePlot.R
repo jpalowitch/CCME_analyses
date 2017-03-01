def094f02dc1776e880076ad6c04afdf8c369696
John Palowitch <johnpalowitch@Johns-iMac.local>
2017-02-28 21:31:40 -0500

    Added functionality to give multi-panel sims plot.

--- makePerformancePlot.R
+++ makePerformancePlot.R
@@ -1,5 +1,10 @@
+tonmi <- function(x, a) {
+  (1 / (1 - x + a) - 1 / (1 + a)) / (1 / a - 1 / (1 + a)) 
+}
+
 makePerformancePlot <-  function (fn = NULL, meanMat, sdMat = NULL,
-                                  legPos = "topright", xvals,
+                                  legPos = "topright", xvals, tnmi = FALSE,
+                                  tnmi_a = 0.05,
                                   legCex = 2, pchs = 1:nrow(meanMat),
                                   lwd = 1, legLwd = 2, yRange = NULL,
                                   cex = 1, new = TRUE, doLegend = TRUE,
@@ -28,6 +33,11 @@ makePerformancePlot <-  function (fn = NULL, meanMat, sdMat = NULL,
     yRange <- c(lowerLimit, upperLimit)
   }
   
+  if (tnmi) {
+    yRange <- c(0, 1)
+    meanMat <- tonmi(meanMat, tnmi_a)
+  }
+  
   plot(0.5, 0.5, col="white", pch='.',
        xlim = xRange, ylim = yRange, ...)
   
@@ -38,6 +48,12 @@ makePerformancePlot <-  function (fn = NULL, meanMat, sdMat = NULL,
       meth = plot_names[j]
       points(xvals, meanMat[meth, ], pch = pchs[j], cex = cex)
       lines(xvals, meanMat[meth, ], lty = j + 1, lwd = lwd)
+      if (tnmi) {
+        abline(h = tonmi(0.95, tnmi_a), lwd = lwd)
+        text(y = tonmi(0.95, tnmi_a), pos = 3, cex = cex.lab,
+             x = paramVec[round(length(paramVec) * 0.75)],
+             labels = c("oNMI = 0.95"))
+      }
       
     } else {
     
@@ -65,7 +81,6 @@ makePerformancePlot <-  function (fn = NULL, meanMat, sdMat = NULL,
       
       legend(x = legPos, legend = plot_names, lty = 1 + 1:length(plot_names),
              cex = legCex, lwd = legLwd, pch = pchs)
-      
       
     } else {
     
